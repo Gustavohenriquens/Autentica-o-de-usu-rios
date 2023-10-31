3c4768f9334315203deb92cfdb36b6200ca60bc9
Gustavo Henrique <Gustavoproject01@gmail.com>
2023-10-31 17:37:20 -0300

    adição de arquivos

--- database/db.js
+++ database/db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
-
 const connectToDb = () => {
-    mongoose.connect (`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ohch4ch.mongodb.net/?retryWrites=true&w=majority`,
+    mongoose.connect (
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ohch4ch.mongodb.net/?retryWrites=true&w=majority`,
 
        {
         useNewUrlParser: true,
